b97d3656bc7200327f879e5147179397833b6e88
adam.velik <velikadam@seznam.cz>
2019-09-11 12:30:31 +0200

    KEY_INIT

--- src/peripherals/keyboard/keyboard.cpp
+++ src/peripherals/keyboard/keyboard.cpp
@@ -10,3 +10,31 @@
  */
 
 #include <Arduino.h>
+int KO0 = 35;
+int KO1 = 32;
+int KO2 = 33;
+int KO3 = 25;
+int KI0 = 26;
+int KI1 = 27;
+int KI2 = 14;
+int KI3 = 12;
+pinMode(KO0, INPUT);
+pinMode(KO1, INPUT);
+pinMode(KO2, INPUT);
+pinMode(KO3, INPUT);
+pinMode(KI0, OUTPUT);
+pinMode(KI1, OUTPUT);
+pinMode(KI2, OUTPUT);
+pinMode(KI3, OUTPUT);
+
+
+
+    void keyRead(void *parameter)
+{
+    pinMode()
+
+        for (;;)
+    {
+        digitalWrite()
+    }
+}
